92fdc368eb746e8cc2a2251c8304e4230bb386af
Luis Bances <luisangelbancesoliden15@gmail.com>
2026-04-11 22:10:14 -0500

    Primer commit: Creacion del proyecto base

---
+++ src/test/java/com/example/Proyecto_DWI/ProyectoDwiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_DWI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
